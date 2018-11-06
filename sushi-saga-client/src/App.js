39d31fea24e46bad0a89c53a777560adafebf547
braj011 <bhagyaraj01@gmail.com>
2018-11-06 13:27:00 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,12 +5,26 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
+
 class App extends Component {
 
+  state = {
+    sushis: []
+  }
+
+  componentDidMount() {
+    return fetch(API)
+      .then(resp => resp.json())
+      .then(sushis => this.setState( {sushis: sushis} ))
+  }
+
   render() {
+
+    const { sushis } = this.state 
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer sushis={sushis}  />
         <Table />
       </div>
     );
